fca13f6e0a55b368968c75cb7e1bf482ae88eade
jonabirde <bearinparadox@gmail.com>
2025-09-04 18:48:56 +0800

    show corresponding image when hovering over courses

--- src/components/map/MapCard.vue
+++ src/components/map/MapCard.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import type { MapExt, MapQuery } from '@/types'
 import { useRouter } from 'vue-router'
 import { toLocal } from '@/utils'
 
 const router = useRouter()
+
+const currentCourseNumber = ref(1)
 
 defineProps<{
   query: MapQuery
@@ -17,17 +20,13 @@ defineProps<{
       <div class="relative hidden lg:block">
         <TheImage
           class="w-64 h-36 rounded-bl-md rounded-tl-md cursor-pointer"
-          :src="`https://github.com/kzglobalteam/cs2kz-images/raw/public/webp/medium/kz_grotto/1.webp`"
+          :src="`https://github.com/kzglobalteam/cs2kz-images/raw/public/webp/medium/${map.name}/${currentCourseNumber}.webp`"
           @click="router.push({ path: `/maps/${map.name}/1` })"
         />
         <IconCheckOutline class="absolute top-1 left-1" v-if="map.state === 'approved'" />
       </div>
 
       <div
-        :style="{
-          backgroundImage: `url(https://github.com/kzglobalteam/cs2kz-images/raw/public/webp/medium/kz_grotto/1.webp)`,
-          backgroundSize: 'cover',
-        }"
         class="w-56 h-36 px-2 pb-1 flex flex-col justify-between rounded-tr-md rounded-br-md relative bg-[length:200%_200%] bg-center before:content-[''] before:absolute before:inset-0 before:rounded-tr-md before:rounded-br-md before:bg-gradient-to-r before:from-[rgba(46,46,46,1)] before:to-[rgba(46,46,46,0.6)] before:z-0 before:transition-all"
       >
         <div class="relative w-full h-full">
@@ -56,6 +55,7 @@ defineProps<{
                 <div
                   class="w-max px-1 flex justify-center items-center gap-1 text-xs rounded-[0.2rem] border border-gray-600 bg-gray-700/80 hover:bg-gray-600 cursor-pointer"
                   @click="router.push({ path: `/maps/${map.name}/${course.local_id}` })"
+                  @mouseenter="currentCourseNumber = course.local_id"
                 >
                   <div
                     class="w-2 h-2 rounded-full"
