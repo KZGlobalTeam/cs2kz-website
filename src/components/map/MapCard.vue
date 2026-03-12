90858caa4d7adcb72af0c6ef6d894dce44164335
jonabirde <jonabirde@gmail.com>
2026-03-12 09:33:31 +0800

    Show filter detail when hovering map courses

--- src/components/map/MapCard.vue
+++ src/components/map/MapCard.vue
@@ -45,22 +45,52 @@ defineProps<{
 
             <div class="courses flex flex-wrap gap-1 max-h-32 overflow-auto">
               <div v-for="course in map.courses" :key="course.name">
-                <RouterLink
-                  :to="`/maps/${map.name}?course=${course.name}`"
-                  class="w-max px-1 flex justify-center items-center gap-1 text-xs rounded-sm border border-gray-600 bg-gray-700/80 hover:bg-gray-600 cursor-pointer"
-                >
-                  <span
-                    class="inline-block w-2 h-2 rounded-full"
-                    :style="{ backgroundColor: course.state === 'ranked' ? '#05df72' : '#d1d5dc' }"
-                  ></span>
-                  <span
-                    :style="{
-                      color: course.tierColor,
-                    }"
-                    >{{ course.tierNo }}</span
+                <UTooltip :ui="{ content: 'h-max' }">
+                  <template #content>
+                    <div class="flex flex-col">
+                      <div>
+                        <span class="text-gray-300">{{ $t('common.sortBy.tier') }}</span>
+                        <span class="mx-1 text-gray-400">:</span>
+                        <span
+                          :style="{
+                            color: course.tierColor,
+                          }"
+                          >{{ course.tierNo }}</span
+                        >
+                        <span class="mx-0.5 text-gray-500">-</span>
+                        <span
+                          :style="{
+                            color: course.tierColor,
+                          }"
+                          >{{ $t(`common.tier.${course.tier}`) }}</span
+                        >
+                      </div>
+                      <div>
+                        <span class="text-gray-300">{{ $t('map.filterState.name') }}</span>
+                        <span class="mx-1 text-gray-400">:</span>
+                        <span :style="{ color: course.state === 'ranked' ? '#05df72' : '#d1d5dc' }">{{
+                          $t(`map.filterState.${course.state}`)
+                        }}</span>
+                      </div>
+                    </div>
+                  </template>
+                  <RouterLink
+                    :to="`/maps/${map.name}?course=${course.name}`"
+                    class="w-max px-1 flex justify-center items-center gap-1 text-xs rounded-sm border border-gray-600 bg-gray-700/80 hover:bg-gray-600 cursor-pointer"
                   >
-                  <span>{{ course.name }}</span>
-                </RouterLink>
+                    <span
+                      class="inline-block w-2 h-2 rounded-full"
+                      :style="{ backgroundColor: course.state === 'ranked' ? '#05df72' : '#d1d5dc' }"
+                    ></span>
+                    <span
+                      :style="{
+                        color: course.tierColor,
+                      }"
+                      >{{ course.tierNo }}</span
+                    >
+                    <span>{{ course.name }}</span>
+                  </RouterLink>
+                </UTooltip>
               </div>
             </div>
           </div>
