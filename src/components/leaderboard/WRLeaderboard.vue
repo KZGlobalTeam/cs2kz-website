430e82a556b7c312dfae6e1f8fab1f8b907dc17f
jonabirde <jonabirde@gmail.com>
2026-05-15 09:11:49 +0800

    Add rank number in wr leaderboard

--- src/components/leaderboard/WRLeaderboard.vue
+++ src/components/leaderboard/WRLeaderboard.vue
@@ -12,17 +12,23 @@ const emits = defineEmits<{
 
 <template>
   <div class="overflow-auto">
-    <div class="grid grid-cols-[1fr_auto] gap-3 px-4 py-2 border-b border-gray-700 text-sm font-semibold text-gray-500">
+    <div
+      class="grid grid-cols-[40px_1fr_auto] gap-3 px-4 py-2 border-b border-gray-700 text-sm font-semibold text-gray-500"
+    >
+      <span>#</span>
       <span>{{ $t('leaderboards.title.player') }}</span>
       <span>{{ $t('leaderboards.wrs.count') }}</span>
     </div>
 
     <ul class="divide-y divide-gray-800">
       <li
-        v-for="entry in leaderboard"
+        v-for="(entry, index) in leaderboard"
         :key="entry.player.id"
-        class="grid grid-cols-[1fr_auto] h-12 gap-3 px-4 py-2 items-center"
+        class="grid grid-cols-[40px_1fr_auto] h-12 gap-3 px-4 py-2 items-center"
       >
+        <span class="text-gray-400">
+          {{ index + 1 }}
+        </span>
         <RouterLink
           :to="`/profile/${entry.player.id}`"
           class="truncate text-cyan-500 hover:text-cyan-400 transition-colors"
@@ -32,10 +38,10 @@ const emits = defineEmits<{
 
         <button
           type="button"
-          class="inline-flex items-center gap-1 rounded-md px-2 text-amber-400 hover:bg-amber-400/10 transition-colors cursor-pointer"
+          class="inline-flex rounded-md text-amber-400 px-2 py-1 hover:bg-amber-400/10 transition-colors cursor-pointer"
           @click="emits('openDrawer', entry.player.id)"
         >
-          <span class="font-semibold">{{ entry.count }}</span>
+          <p class="text-right font-semibold">{{ entry.count }}</p>
           <IconMedalFirst />
         </button>
       </li>
